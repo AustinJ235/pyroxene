9fe35b3a4273227f7886cbef1b88279f31faeebf
Austin Johnson <me@austinj.work>
2022-09-03 23:32:12 -0500

    unify rustfmt across projects

--- src/category.rs
+++ src/category.rs
@@ -1,31 +1,40 @@
-use crate::desktop::DesktopEntry;
 use std::sync::Arc;
+
+use crate::desktop::DesktopEntry;
 
 #[derive(Debug)]
 pub struct Category {
-	pub iden: String,
-	pub name: String,
-	pub icon: String,
-	pub entries: Vec<Arc<DesktopEntry>>,
+    pub iden: String,
+    pub name: String,
+    pub icon: String,
+    pub entries: Vec<Arc<DesktopEntry>>,
 }
 
 impl Category {
-	pub fn new<Id: Into<String>, N: Into<String>, Ic: Into<String>>(iden: Id, name: N, icon: Ic) -> Self {
-		Category {
-			iden: iden.into(),
-			name: name.into(),
-			icon: icon.into(),
-			entries: Vec::new(),
-		}
-	}
+    pub fn new<Id: Into<String>, N: Into<String>, Ic: Into<String>>(
+        iden: Id,
+        name: N,
+        icon: Ic,
+    ) -> Self {
+        Category {
+            iden: iden.into(),
+            name: name.into(),
+            icon: icon.into(),
+            entries: Vec::new(),
+        }
+    }
 
-	pub fn add_entries(&mut self, entries: &Vec<Arc<DesktopEntry>>) {
-		for entry in entries {
-			if entry.categories.iter().any(|c| c.to_lowercase() == self.iden.to_lowercase()) {
-				self.entries.push(entry.clone());
-			}
-		}
+    pub fn add_entries(&mut self, entries: &Vec<Arc<DesktopEntry>>) {
+        for entry in entries {
+            if entry
+                .categories
+                .iter()
+                .any(|c| c.to_lowercase() == self.iden.to_lowercase())
+            {
+                self.entries.push(entry.clone());
+            }
+        }
 
-		self.entries.sort_by_key(|e| e.name.to_lowercase());
-	}
+        self.entries.sort_by_key(|e| e.name.to_lowercase());
+    }
 }
